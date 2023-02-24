362334bfeff80d39cca3d8651f5ffd04b016d9f1
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-02-23 19:36:26 -0800

    create registration bcg  title inputs

---
+++ screens/auth/LoginScreen.js
@@ -0,0 +1,45 @@
+
+// import React from "react";
+// import {
+//   StyleSheet,
+ 
+//   View,
+//   ImageBackground,
+//   TextInput,
+// } from "react-native";
+
+
+// export default function LoginScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <ImageBackground
+//         style={styles.image}
+//         source={require("../../assets/Images/bcgRegistrationLg.png")}
+//       >
+//         <TextInput style={styles.input} textAlign={"center"} />
+//       </ImageBackground>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//   },
+//   image: {
+//     flex: 1,
+//     resizeMode: "cover",
+//     height: 812,
+//     width: 375,
+//   },
+//   input: {
+//     borderWidth: 1,
+//     borderColor: "#f0f8ff",
+//     height: 40,
+//     borderRadius: 6,
+//     marginHorizontal: 40,
+//     color: "#f0f8ff",
+//   },
+// });
+
---
+++ screens/auth/RegistrationScreen.js
@@ -0,0 +1,34 @@
+// import React from "react";
+// import { StatusBar } from "expo-status-bar";
+
+// import { StyleSheet, View, ImageBackground } from "react-native";
+
+// export default function RegistrationScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <ImageBackground
+//         style={styles.image}
+//         source={require("./assets/Images/bcgRegistrationLg.png")}
+//       >
+//         {/* <Text>Hello Word!</Text>
+//         <Text>Open up App.js to start working on your App!</Text> */}
+//         <StatusBar style="auto" />
+//       </ImageBackground>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     // alignItems: "center",
+//     // justifyContent: "center",
+//   },
+//   image: {
+//     flex: 1,
+//     resizeMode: "cover",
+//     // height: 812,
+//     // width: 375,
+//   },
+// });
